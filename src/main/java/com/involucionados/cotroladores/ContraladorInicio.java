a14aafd5f2ce57429af0454fcb09d19aa8e72a05
cristobal <c.cifuentes1990@gmail.com>
2020-07-28 22:57:45 -0400

    commit previo push

--- src/main/java/com/involucionados/cotroladores/ContraladorInicio.java
+++ src/main/java/com/involucionados/cotroladores/ContraladorInicio.java
@@ -2,24 +2,21 @@ package com.involucionados.cotroladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
-=======
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
->>>>>>> gerald
+
 
 @Controller
 public class ContraladorInicio {
 	
-	/*
-	 * @GetMapping(value={"/home", "", "/", "/index"}) public String inicio() {
-	 * return "index"; }
-	 */
 	
-	@GetMapping("/")
-	public String index() {
-		return "index1";
-	}
+	  @GetMapping(value={"/home", "", "/", "/index"}) 
+	  public String inicio() {
+	  return "index"; }
+	  
+	  @GetMapping(value="/index1") 
+	  public String inicio2() {
+	  return "index1"; }
+	 
 	
+
 	
 }
